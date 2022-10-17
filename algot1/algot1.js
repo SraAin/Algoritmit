b6b0f0129be7ac1c615e4205d2cb7021e4db7c5a
Sara Ainali <sara.ainali@gmail.com>
2022-10-17 14:18:41 +0300

    First commit

---
+++ algot1/algot1.js
@@ -0,0 +1,34 @@
+const rl = require('readline-sync');
+
+/*
+1. Luodaan tyhjä merkkijono
+2. Kysytään merkkiä niin kauan kun merkki ei ole #
+3. Jos merkki ei ole #, lisätään se merkkijonoon, muuten poistutaan silmukasta
+4. Silmukasta poistumisen jälkeen palautetaan merkkijono.
+*/
+
+function collectFeed() {
+  // luodaan tyhjä merkkijono
+  let str = '';
+  let char;
+
+  // Niin kauan kuin merkki ei ole '#'
+  while (char !== '#') {
+    char = rl.question('Anna merkki: ');
+    // Jos merkki ei ole '#', lisätään se merkkijonoon
+    if (char !== '#') {
+      str = str + char;
+    } else {
+      // Poistutaan silmukasta
+      break;
+    }
+  }
+
+  // Palautetaan merkkijono
+  return str;
+}
+
+const mj = collectFeed();
+console.log('Palautettu merkkijono: ' + mj);
+
+module.exports = { collectFeed };
